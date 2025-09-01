cecb13659364e2689ef233051439ecefa590c717
JakobPogacnikSouvent <jakob.pogacnik.s@gmail.com>
2025-09-01 19:49:01 +0200

    Rsync setup script & input

--- hypr/scripts/set-submap.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-echo "$1" > /tmp/hypr_current_submap
-hyprctl dispatch submap $1
--- setup.sh
+++ setup.sh
@@ -1,40 +1,45 @@
 #!/bin/bash
 
-# Prompt for input
 read -p "Enter device type ((1)desktop / (2)laptop): " device
 
-mkdir -p ~/.config/hypr
-mkdir -p ~/.config/hypr/scripts
-mkdir -p ~/.config/wallpapers
-mkdir -p ~/.config/waybar
+sync_folder() {
+    local src="$1/"
+    local dest="$HOME/.config/$1/"
+    mkdir -p "$dest"
+    rsync -a --delete "$src" "$dest"
+}
 
-# Check the input
+sync_folder "alacritty"
+sync_folder "nvim"
+sync_folder "wofi"
+sync_folder "neofetch"
+
+mkdir -p "$HOME/.config/hypr"
+mkdir -p "$HOME/.config/waybar"
+
+rsync -a --delete --exclude 'desktop/' --exclude 'laptop/' hypr/ "$HOME/.config/hypr/"
+rsync -a --delete --exclude 'desktop/' --exclude 'laptop/' waybar/ "$HOME/.config/waybar/"
+
+
 if [[ "$device" == "desktop" || "$device" == "1" ]]; then
-    echo "You selected desktop. Performing desktop-related tasks..."
-    cp ~/dotfiles/hypr/desktop/monitors.conf ~/.config/hypr/
-    cp ~/dotfiles/hypr/desktop/scripts/cycle-workspace.sh ~/.config/hypr/scripts/
-    cp ~/dotfiles/waybar/desktop/config.jsonc ~/.config/waybar/
+    echo "Syncing desktop configs..."
+    rsync -a hypr/desktop/monitors.conf "$HOME/.config/hypr/"
+    rsync -a hypr/desktop/input.conf "$HOME/.config/hypr/"
+    
+    rsync -a hypr/desktop/scripts/cycle-workspace.sh "$HOME/.config/hypr/scripts/"
+    
+    rsync -a waybar/desktop/config.jsonc "$HOME/.config/waybar/"
 elif [[ "$device" == "laptop" || "$device" == "2" ]]; then
-    echo "You selected laptop. Performing laptop-related tasks..."
-    cp ~/dotfiles/hypr/laptop/monitors.conf ~/.config/hypr/
-    cp ~/dotfiles/hypr/laptop/scripts/cycle-workspace.sh ~/.config/hypr/scripts/
-    cp ~/dotfiles/waybar/laptop/config.jsonc ~/.config/waybar/
+    echo "Syncing laptop configs..."
+    rsync -av hypr/laptop/monitors.conf "$HOME/.config/hypr/"
+    rsync -a hypr/laptop/input.conf "$HOME/.config/hypr/"
+
+    rsync -a hypr/laptop/scripts/cycle-workspace.sh "$HOME/.config/hypr/scripts/"
+    
+    rsync -a waybar/laptop/config.jsonc "$HOME/.config/waybar/"
 else
     echo "Error: Invalid device type. Please enter 'desktop' or 'laptop'."
     exit 1
 fi
 
-cp ~/dotfiles/hypr/*.conf ~/.config/hypr/
-cp -r ~/dotfiles/hypr/scripts ~/.config/hypr/
-
-cp ~/dotfiles/wallpapers/wallhaven-exl22k.jpg ~/.config/wallpapers/
-
-cp -r ~/dotfiles/alacritty ~/.config/alacritty
-
-cp -r ~/dotfiles/nvim ~/.config/nvim
-
-cp ~/dotfiles/waybar/*.css ~/.config/waybar/
-
-cp -r ~/dotfiles/wofi ~/.config/wofi
-
-cp -r ~/dotfiles/neofetch ~/.config/neofetch
+hyprctl reload
