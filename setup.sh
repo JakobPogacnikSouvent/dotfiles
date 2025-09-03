e3f96af38460358b433ac46725e8c34231f26433
JakobPogacnikSouvent <jakob.pogacnik.s@gmail.com>
2025-09-03 17:05:21 +0200

    Zote in wofi & cleaner setup

--- setup.sh
+++ setup.sh
@@ -2,48 +2,52 @@
 
 read -p "Enter device type ((1)desktop / (2)laptop): " device
 
+case "$device" in
+    1 | desktop) device="desktop" ;;
+    2 | laptop)  device="laptop"  ;;
+    *) 
+        echo "Error: Invalid device type. Please enter 'desktop' or 'laptop'."
+        exit 1
+        ;;
+esac
+
 sync_folder() {
-    local src="$1/"
+    local src="$HOME/dotfiles/$1/"
     local dest="$HOME/.config/$1/"
     mkdir -p "$dest"
     rsync -a --delete "$src" "$dest"
 }
+
+### General configs ###
 
 sync_folder "alacritty"
 sync_folder "nvim"
 sync_folder "wofi"
 sync_folder "neofetch"
 
+# wallpaper
 mkdir -p "$HOME/.config/wallpapers"
-rsync -a wallpapers/wallhaven-exl22k.jpg "$HOME/.config/wallpapers"
+rsync -a "$HOME/dotfiles/wallpapers/wallhaven-exl22k.jpg" "$HOME/.config/wallpapers"
 
-
+# hypr
 mkdir -p "$HOME/.config/hypr"
+rsync -a --delete --exclude 'desktop/' --exclude 'laptop/' "$HOME/dotfiles/hypr/" "$HOME/.config/hypr/"
+
+# waybar
+mkdir -p "$HOME/.config/waybar"
+rsync -a --delete --exclude 'desktop/' --exclude 'laptop/' "$HOME/dotfiles/waybar/" "$HOME/.config/waybar/"
+
+### Device Specific configs ###
+
+# hypr
+mkdir -p "$HOME/.config/hypr/scripts"
 mkdir -p "$HOME/.config/waybar"
 
-rsync -a --delete --exclude 'desktop/' --exclude 'laptop/' hypr/ "$HOME/.config/hypr/"
-rsync -a --delete --exclude 'desktop/' --exclude 'laptop/' waybar/ "$HOME/.config/waybar/"
+rsync -a "$HOME/dotfiles/hypr/$device/monitors.conf" "$HOME/.config/hypr/"
+rsync -a "$HOME/dotfiles/hypr/$device/input.conf" "$HOME/.config/hypr/"
+rsync -a "$HOME/dotfiles/hypr/$device/scripts/cycle-workspace.sh" "$HOME/.config/hypr/scripts/"
 
-
-if [[ "$device" == "desktop" || "$device" == "1" ]]; then
-    echo "Syncing desktop configs..."
-    rsync -a hypr/desktop/monitors.conf "$HOME/.config/hypr/"
-    rsync -a hypr/desktop/input.conf "$HOME/.config/hypr/"
-    
-    rsync -a hypr/desktop/scripts/cycle-workspace.sh "$HOME/.config/hypr/scripts/"
-    
-    rsync -a waybar/desktop/config.jsonc "$HOME/.config/waybar/"
-elif [[ "$device" == "laptop" || "$device" == "2" ]]; then
-    echo "Syncing laptop configs..."
-    rsync -av hypr/laptop/monitors.conf "$HOME/.config/hypr/"
-    rsync -a hypr/laptop/input.conf "$HOME/.config/hypr/"
-
-    rsync -a hypr/laptop/scripts/cycle-workspace.sh "$HOME/.config/hypr/scripts/"
-    
-    rsync -a waybar/laptop/config.jsonc "$HOME/.config/waybar/"
-else
-    echo "Error: Invalid device type. Please enter 'desktop' or 'laptop'."
-    exit 1
-fi
+# waybar
+rsync -a "$HOME/dotfiles/waybar/$device/config.jsonc" "$HOME/.config/waybar/"
 
 hyprctl reload
--- wofi/wofi-random-prompt.sh
+++ wofi/wofi-random-prompt.sh
@@ -1,78 +1,10 @@
 #!/bin/bash
 
-# Define your list of random words
-frog_noises=(
-  "ribbit" "croak" "gribbit" "brrr-up" "reep-reep"
-  "chirrup" "quonk" "rrraaaack" "peep" "churr"
-  "kraak" "waaaaak" "urp" "jug-o-rum" "bleep"
-  "glug" "bloop" "gunk" "brup" "rrip-it"
-)
-
-precepts=(
-    "Always Win Your Battles"
-    "Never Let Them Laugh at You"
-    "Always Be Rested"
-    "Forget Your Past"
-    "Strength Beats Strength"
-    "Choose Your Own Fate"
-    "Mourn Not the Dead"
-    "Travel Alone"
-    "Keep Your Home Tidy"
-    "Keep Your Weapon Sharp"
-    "Mothers Will Always Betray You"
-    "Keep Your Cloak Dry"
-    "Never Be Afraid"
-    "Respect Your Superiors"
-    "One Foe, One Blow"
-    "Don't Hesitate"
-    "Believe In Your Strength"
-    "Seek Truth in the Darkness"
-    "If You Try, Succeed"
-    "Speak Only the Truth"
-    "Be Aware of Your Surroundings"
-    "Abandon the Nest"
-    "Identify the Foe"
-    "Strike the Foe"
-    "Protect Your Own Weak Point"
-    "Don't Trust Your Reflection"
-    "Eat As Much As You Can"
-    "Don't Peer Into the Darkness"
-    "Develop Your Sense of Direction"
-    "Never Accept a Promise"
-    "Disease Lives Inside of Dirt"
-    "Names Have Power"
-    "Show the Enemy No Respect"
-    "Don't Eat Immediately Before Sleeping"
-    "Up is Up, Down is Down"
-    "Eggshells are brittle"
-    "Borrow, But don't Lend"
-    "Beware the Mysterious Force"
-    "Eat Quickly and Drink Slowly"
-    "Obey No Law But Your Own"
-    "Learn to Detect Lies"
-    "Spend Geo When You Have It"
-    "Never Forgive"
-    "You Can Not Breathe Water"
-    "One Thing Is Not Another"
-    "The World is Smaller Than You Think"
-    "Make Your Own Weapon"
-    "Be Careful With Fire"
-    "Statues are Meaningless"
-    "Don't Linger on Mysteries"
-    "Nothing is Harmless"
-    "Beware the Jealousy of Fathers"
-    "Don't Steal the Desires of Others"
-    "If You Lock Something Away, Keep the Key"
-    "Bow to No-one"
-    "Don't Dream"
-    "Obey All Precepts"
-)
-
-rand=$((RANDOM%${#precepts[@]}))
+precept=$(~/.local/bin/zote)
 
 if pgrep -x "wofi" > /dev/null; then
 # wofy already running
   exit 0
 else
-  wofi -p "${precepts[rand]}"
+  wofi -p "${precept}"
 fi
